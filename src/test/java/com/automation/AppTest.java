c6c70c9ddd33321d5ecff95d3b3af3f411bb9e8f
Priyanshu T <76653144+Blank-09@users.noreply.github.com>
2024-04-29 00:13:47 +0530

    Update Logger Text and code formatted

--- src/test/java/com/automation/AppTest.java
+++ src/test/java/com/automation/AppTest.java
@@ -37,26 +37,26 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class AppTest {
-    
 
     private final String CHATGPT_URL = "https://chat.openai.com/";
 
     // Update the path to your Chrome profile directory
     private final String EXECUTABLE_PATH = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
-    private final String USER_DATA_DIR = "C:\\Users\\<UserName>\\AppData\\Local\\Google\\Chrome\\User Data";
+    private final String USER_DATA_DIR = "C:\\Users\\<username>\\AppData\\Local\\Google\\Chrome\\User Data";
     private final String PROFILE_DIRECTORY = "Profile 1";
 
     private final String QUESTION_SHEET_PATH = "./assets/sheets/questions.xlsx";
     private final String REPORT_PATH = "./out/reports/index.html";
     private final String LOGGER_PATH = "./out/logs/app.log";
     private final String SCREENSHOT_PATH = "./out/screenshots/";
-    Logger logger = LogManager.getLogger(getClass());
 
     WebDriver driver;
     Actions actions;
     ExtentReports reports;
     Wait<WebDriver> wait;
     List<Question> questions;
+
+    java.util.logging.Logger logger = LogManager.getLogger(getClass());
 
     public class Question {
 
@@ -87,22 +87,24 @@ public class AppTest {
     public void setupDriver() {
         logger.info("Setting up Chrome driver...");
         logger.info("Creating ChromeOptions object...");
+
         ChromeOptions options = new ChromeOptions();
-        logger.info("Setting executable path...");
+
+        logger.info("Setting executable path to " + EXECUTABLE_PATH);
         options.setBinary(EXECUTABLE_PATH);
-        logger.info("Adding user data directory argument...");
+
+        logger.info("Setting user data directory to " + USER_DATA_DIR);
         options.addArguments("--user-data-dir=" + USER_DATA_DIR);
-        logger.info("Adding profile directory argument...");
+
+        logger.info("Setting profile directory to " + PROFILE_DIRECTORY);
         options.addArguments("--profile-directory=" + PROFILE_DIRECTORY);
 
-        logger.info("Creating ChromeDriver object...");
+        logger.info("Creating ChromeDriver object.");
         this.driver = new ChromeDriver(options);
-        logger.info("Creating Actions object...");
         this.actions = new Actions(driver);
-        logger.info("Creating WebDriverWait object...");
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        logger.info("Chrome driver setup complete.");
 
+        logger.info("Chrome driver setup complete.");
     }
 
     @BeforeTest
@@ -127,71 +129,79 @@ public class AppTest {
     @Test(priority = 1)
     public void getAnswersFromChat() throws InterruptedException {
 
-        logger.info("Getting answers from chat...");
-    try {
-        logger.info("Navigating to ChatGPT URL...");
-        driver.get(CHATGPT_URL);
+        logger.info("Initializing Testcase 1");
+        logger.info("Preparing to initialize ChatGPT");
 
-        logger.info("Creating WebDriverWait object...");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(200));
+        try {
+            logger.info("Navigating to " + CHATGPT_URL);
+            driver.get(CHATGPT_URL);
 
-        logger.info("Creating textareaLocator object...");
-        By textareaLocator = By.id("prompt-textarea");
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(200));
 
-        logger.info("Creating submitButtonLocator object...");
-        By submitButtonLocator = By.cssSelector("button[data-testid=send-button]");
+            By textareaLocator = By.id("prompt-textarea");
+            By submitButtonLocator = By.cssSelector("button[data-testid=send-button]");
 
-        for (Question question : questions) {
-            logger.info("Getting question text...");
-            String questionText = question.question;
+            for (Question question : questions) {
+                logger.log("Querying question " + question.sno + " (" + question.marks + " marks)");
 
-            logger.info("Creating marks string...");
-            String marks = ". Answer the question as " + question.marks + " marks";
+                String questionText = question.question;
+                String marks = ". Answer the question as " + question.marks + " marks";
+                String additionalInfo = "";
 
-            String additionalInfo = "";
+                if (question.additionalInfo != null && !question.additionalInfo.isEmpty()) {
+                    logger.info("Adding additional information...");
+                    additionalInfo = " and add the following information: " + question.additionalInfo;
+                } else {
+                    logger.warning("Additional Information not provided. Please provide it for better results.");
+                }
 
-            if (question.additionalInfo!= null &&!question.additionalInfo.isEmpty()) {
-                logger.info("Adding additional information...");
-                additionalInfo = " and add the following information: " + question.additionalInfo;
+                String prompt = questionText + marks + additionalInfo;
+                logger.log("Prompt: " + prompt);
+
+                // Entering question text into the text area
+                logger.info("Entering question text into textarea...");
+                driver.findElement(textareaLocator).sendKeys(prompt);
+
+                // Clicking the button to submit the question
+                logger.info("Clicking submit button...");
+                driver.findElement(submitButtonLocator).click();
+
+                // Waiting for the button to disappear and then reappear
+                logger.info("Waiting for submit button to disappear...");
+                wait.until(ExpectedConditions.invisibilityOfElementLocated(submitButtonLocator));
+
+                logger.info("Waiting for submit button to reappear...");
+                wait.until(ExpectedConditions.presenceOfElementLocated(submitButtonLocator));
+
+                logger.info("Waiting for 5 seconds...");
+                Thread.sleep(5000);
             }
-
-            logger.info("Creating prompt string...");
-            String prompt = questionText + marks + additionalInfo;
-
-            // Entering question text into the text area
-            logger.info("Entering question text into text area...");
-            driver.findElement(textareaLocator).sendKeys(prompt);
-
-            // Clicking the button to submit the question
-            logger.info("Clicking submit button...");
-            driver.findElement(submitButtonLocator).click();
-
-            // Waiting for the button to disappear and then reappear
-            logger.info("Waiting for submit button to disappear...");
-            wait.until(ExpectedConditions.invisibilityOfElementLocated(submitButtonLocator));
-            logger.info("Waiting for submit button to reappear...");
-            wait.until(ExpectedConditions.presenceOfElementLocated(submitButtonLocator));
-            logger.info("Waiting for 5 seconds...");
-            Thread.sleep(5000);
+        } catch (Exception e) {
+            logger.error("An error occurred while getting answers from Chat: ", e);
         }
-    } catch (Exception e) {
-        logger.error("An error occurred while getting answers from chat: ", e);
-    }
-    logger.info("Getting answers from chat complete.");
+
+        logger.info("ChatGPT Automation complete.");
     }
 
     @AfterTest
     public void wrapUp() {
+        logger.info("Wrapping up...");
+        logger.info("Quitting WebDriver");
         driver.quit();
-        logger.info("Quitting Driver");
         // reports.flush();
+        logger.info("Wrap-up complete");
     }
 
     // All your private function goes here lexigraphically
     private void takeScreenshot(String name) throws IOException {
+        logger.info("Taking Screenshot...");
+
+        String screenshotPath = SCREENSHOT_PATH + name + "_" + timestamp + ".png";
         File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
-        FileUtils.copyFile(screenshotFile, new File(SCREENSHOT_PATH + name + "_" + timestamp + ".png"));
+
+        FileUtils.copyFile(screenshotFile, new File(screenshotPath));
+        logger.info("Screenshot saved at " + screenshotPath);
     }
 
 }
